66d6e8f0711bfc33c75519261ad381a1e1119fd9
James-Burch <burchjames6@gmail.com>
2025-06-11 02:39:31 +0100

    Update app tsx to include new styles

--- AK-Site/src/App.tsx
+++ AK-Site/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Home from './pages/Home'
 import './styles/globals.css'
 import './styles/components.css'
-import './styles/pages.css'
+import './styles/mortgage.css'
 
 function App() {
   return <Home />
