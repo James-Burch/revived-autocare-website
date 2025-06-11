862375c160b8723ea2b61f6e2e6524d4a060dd61
James-Burch <burchjames6@gmail.com>
2025-06-11 02:12:10 +0100

    Create component index file

---
+++ AK-Site/src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Counter } from './Counter';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as Layout } from './Layout';
+export { default as Loading } from './Loading';
+export { default as Modal } from './Modal';
