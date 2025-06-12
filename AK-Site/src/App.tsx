4860c0efd02695b8520e922e054e199ff06701b3
James-Burch <burchjames6@gmail.com>
2025-06-12 07:35:50 +0100

    Updated app file and css for new pages

--- AK-Site/src/App.tsx
+++ AK-Site/src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import Home from './pages/Home'
-import './styles/globals.css'
-import './styles/components.css'
-import './styles/mortgage.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import './styles/globals.css';
+import './styles/components.css';
+import './styles/mortgage.css';
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
