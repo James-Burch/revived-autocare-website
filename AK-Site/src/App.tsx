4562a8584d14748947b70e784ae6bbe1fe8ee486
James-Burch <burchjames6@gmail.com>
2025-07-28 13:09:20 +0100

    Add new product routes to app.tsx

--- AK-Site/src/App.tsx
+++ AK-Site/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Contact from './pages/Contact';
+import ProductPage from './components/ProductPage';
 import './styles/globals.css';
 import './styles/components.css';
 import './styles/mortgage.css';
@@ -13,6 +14,18 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
+        
+        {/* Individual Product Pages */}
+        <Route path="/products/first-time-buyers" element={<ProductPage />} />
+        <Route path="/products/home-mover" element={<ProductPage />} />
+        <Route path="/products/remortgage" element={<ProductPage />} />
+        <Route path="/products/buy-to-let" element={<ProductPage />} />
+        <Route path="/products/new-build" element={<ProductPage />} />
+        <Route path="/products/help-to-buy" element={<ProductPage />} />
+        <Route path="/products/limited-companies" element={<ProductPage />} />
+        
+        {/* Calculators Page */}
+        <Route path="/calculators" element={<ProductPage />} />
       </Routes>
     </Router>
   );
